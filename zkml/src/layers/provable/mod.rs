be6793f7dfa4837d1e28f970eabd469e7e4328c7
Zack Youell <zackyouell@mac.com>
2025-05-27 11:49:09 +0100

    Batch opening with graph model

--- zkml/src/layers/provable/mod.rs
+++ zkml/src/layers/provable/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::{Result, anyhow, bail, ensure};
 use ff_ext::ExtensionField;
+use mpcs::PolynomialCommitmentScheme;
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use std::{
     collections::{BTreeMap, HashMap},
@@ -8,7 +9,7 @@ use std::{
 use transcript::Transcript;
 
 use crate::{
-    Claim, Element, Prover, ScalingFactor, ScalingStrategy, Tensor,
+    Claim, Context, Element, Prover, ScalingFactor, ScalingStrategy, Tensor,
     commit::precommit::PolyID,
     iop::{
         context::{ContextAux, ShapeStep},
@@ -20,7 +21,10 @@ use crate::{
     tensor::{ConvData, Number},
 };
 
-use super::{Layer, LayerCtx, LayerProof, flatten::Flatten, requant::Requant};
+use super::{
+    Layer, LayerCtx, LayerProof, convolution::ConvCtx, dense::DenseCtx, flatten::Flatten,
+    requant::Requant,
+};
 
 pub(crate) type NodeId = usize;
 
@@ -314,13 +318,14 @@ pub trait PadOp {
     }
 }
 
-pub trait ProvableOp<E>: OpInfo + PadOp + ProveInfo<E>
+pub trait ProvableOp<E, PCS>: OpInfo + PadOp + ProveInfo<E>
 where
     E: ExtensionField,
     E::BaseField: Serialize + DeserializeOwned,
     E: Serialize + DeserializeOwned,
+    PCS: PolynomialCommitmentScheme<E>,
 {
-    type Ctx: VerifiableCtx<E>;
+    type Ctx: VerifiableCtx<E, PCS>;
 
     /// Produces a proof of correct execution for this operation.
     fn prove<T: Transcript<E>>(
@@ -329,7 +334,7 @@ where
         _ctx: &Self::Ctx,
         _last_claims: Vec<&Claim<E>>,
         _step_data: &StepData<E, E>,
-        _prover: &mut Prover<E, T>,
+        _prover: &mut Prover<E, T, PCS>,
     ) -> Result<Vec<Claim<E>>> {
         // Default implementation, to avoid having to implement this method in case `is_provable` is false
         assert!(
@@ -343,7 +348,8 @@ where
     fn gen_lookup_witness(
         &self,
         _id: NodeId,
-        _gen: &mut LookupWitnessGen<E>,
+        _gen: &mut LookupWitnessGen<E, PCS>,
+        _ctx: &Context<E, PCS>,
         _step_data: &StepData<Element, E>,
     ) -> Result<()> {
         // Default implementation for nodes that don't employ a lookup table
@@ -351,11 +357,12 @@ where
     }
 }
 
-pub trait VerifiableCtx<E>: Debug + OpInfo
+pub trait VerifiableCtx<E, PCS>: Debug + OpInfo
 where
     E: ExtensionField,
     E::BaseField: Serialize + DeserializeOwned,
     E: Serialize + DeserializeOwned,
+    PCS: PolynomialCommitmentScheme<E>,
 {
     type Proof: Sized;
 
@@ -364,7 +371,7 @@ where
         &self,
         proof: &Self::Proof,
         last_claims: &[&Claim<E>],
-        verifier: &mut Verifier<E, T>,
+        verifier: &mut Verifier<E, T, PCS>,
         shape_step: &ShapeStep,
     ) -> Result<Vec<Claim<E>>>;
 }
@@ -431,31 +438,43 @@ where
     }
 }
 
-impl<E: ExtensionField> VerifiableCtx<E> for LayerCtx<E>
+impl<E: ExtensionField, PCS: PolynomialCommitmentScheme<E>> VerifiableCtx<E, PCS> for LayerCtx<E>
 where
     E::BaseField: Serialize + DeserializeOwned,
     E: Serialize + DeserializeOwned,
 {
-    type Proof = LayerProof<E>;
+    type Proof = LayerProof<E, PCS>;
 
     fn verify<T: Transcript<E>>(
         &self,
-        proof: &LayerProof<E>,
+        proof: &LayerProof<E, PCS>,
         last_claims: &[&Claim<E>],
-        verifier: &mut Verifier<E, T>,
+        verifier: &mut Verifier<E, T, PCS>,
         shape_step: &ShapeStep,
     ) -> Result<Vec<Claim<E>>> {
         match self {
             LayerCtx::Dense(dense_ctx) => {
                 if let LayerProof::Dense(proof) = proof {
-                    dense_ctx.verify(proof, last_claims, verifier, shape_step)
+                    <DenseCtx<E> as VerifiableCtx<E, PCS>>::verify(
+                        dense_ctx,
+                        proof,
+                        last_claims,
+                        verifier,
+                        shape_step,
+                    )
                 } else {
                     bail!("dense proof not found when verifying dense layer")
                 }
             }
             LayerCtx::Convolution(conv_ctx) => {
                 if let LayerProof::Convolution(proof) = proof {
-                    conv_ctx.verify(proof, last_claims, verifier, shape_step)
+                    <ConvCtx<E> as VerifiableCtx<E, PCS>>::verify(
+                        conv_ctx,
+                        proof,
+                        last_claims,
+                        verifier,
+                        shape_step,
+                    )
                 } else {
                     bail!("conv proof not found when verifying convolution layer")
                 }
