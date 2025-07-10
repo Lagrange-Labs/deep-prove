83334ea41d8a825e7c593fc963415d1a7f662100
Tomáš Zemanovič <tomas@lagrange.dev>
2025-07-10 08:31:36 +0100

    handle missing model's graph

--- zkml/src/parser/mod.rs
+++ zkml/src/parser/mod.rs
@@ -104,7 +104,9 @@ const RESHAPE: [&str; 2] = ["Flatten", "Reshape"];
 
 fn is_mlp(model: &ModelProto) -> Result<bool> {
     let mut prev_was_gemm_or_matmul = false;
-    let graph = model.graph.as_ref().unwrap();
+    let Some(graph) = model.graph.as_ref() else {
+        bail!("Model has no a graph")
+    };
 
     for node in graph.node.iter() {
         if LINEAR_ALG.contains(&node.op_type.as_str()) {
@@ -132,7 +134,9 @@ fn is_cnn(model: &ModelProto) -> Result<bool> {
     let mut is_cnn = true;
     let mut found_lin = false;
 
-    let graph = model.graph.as_ref().unwrap();
+    let Some(graph) = model.graph.as_ref() else {
+        bail!("Model has no a graph")
+    };
     let mut previous_op = "";
 
     for node in graph.node.iter() {
